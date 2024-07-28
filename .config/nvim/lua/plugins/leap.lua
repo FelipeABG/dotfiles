2575f63d155fbdd12d34a62488b9b5031a44c8a4
felipe <felipe.baleche2@gmail.com>
2024-07-28 18:13:32 -0300

    small changes

--- .config/nvim/lua/plugins/leap.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"ggandor/leap.nvim",
-
-	vim.keymap.set("n", "m", "<Plug>(leap)"),
-
-	vim.api.nvim_create_autocmd("BufWinEnter", { command = "highlight LeapBackdrop guifg=#777777" }),
-	vim.api.nvim_create_autocmd("BufWinEnter", { command = "highlight LeapLabelPrimary guifg=#ff69b4" }),
-}
