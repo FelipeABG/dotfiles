391c49043543ced7d8561e7fb6d8b8ed48ff1ffa
felipe <felipe.baleche2@gmail.com>
2024-08-03 13:10:42 -0300

    changes to nvim colorscheme and alacritty configuration

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,10 +8,12 @@ return {
       disable_background = true,
     }
   end,
-
   -- optionally set the colorscheme within lazy config
   init = function()
     vim.cmd("colorscheme poimandres")
+    vim.cmd("hi! @constructor guifg=#f1e05a")
+    vim.cmd("hi! Identifier guifg=#e4f0fb")
+    vim.cmd("hi! @property guifg=#e4f0fb")
+    vim.cmd("hi! @boolean guifg=#d0679d")
   end
 }
-
