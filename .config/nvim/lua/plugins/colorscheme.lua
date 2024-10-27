62e5d34bb9022d39e94c60596190e758a1e791aa
felipe <felipe.baleche2@gmail.com>
2024-10-27 16:50:45 -0300

    changes to nvim configuration

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
-  "loctvl842/monokai-pro.nvim",
-  priority = 1000,
-  config = function()
-    require("monokai-pro").setup({
-      transparent_background = true,
-      terminal_colors = true,
-      background_clear = {
-        "float_win",
-        "telescope",
-      },
-    })
-    vim.cmd("colorscheme monokai-pro")
-  end
+	"loctvl842/monokai-pro.nvim",
+	priority = 1000,
+	config = function()
+		require("monokai-pro").setup({
+			transparent_background = true,
+			terminal_colors = true,
+			background_clear = {
+				"float_win",
+				"telescope",
+			},
+		})
+		vim.cmd("colorscheme monokai-pro")
+	end,
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,167 +1,166 @@
 return {
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v2.x",
-  dependencies = {
-    { "neovim/nvim-lspconfig" },             -- Required
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" }, -- Optional
-    { "hrsh7th/nvim-cmp" },                  -- Required
-    { "hrsh7th/cmp-nvim-lsp" },              -- Required
-    { "L3MON4D3/LuaSnip" },                  -- Required
-    { "rafamadriz/friendly-snippets" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "hrsh7th/cmp-cmdline" },
-    { "saadparwaiz1/cmp_luasnip" },
-    { "onsails/lspkind-nvim" },
-  },
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v2.x",
+	dependencies = {
+		{ "neovim/nvim-lspconfig" }, -- Required
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+		{ "hrsh7th/nvim-cmp" }, -- Required
+		{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+		{ "L3MON4D3/LuaSnip" }, -- Required
+		{ "rafamadriz/friendly-snippets" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "hrsh7th/cmp-cmdline" },
+		{ "saadparwaiz1/cmp_luasnip" },
+		{ "onsails/lspkind-nvim" },
+	},
 
-  config = function()
-    local lsp = require("lsp-zero")
-    local lspkind = require("lspkind")
-    local window = require("lspconfig.ui.windows")
+	config = function()
+		local lsp = require("lsp-zero")
+		local lspkind = require("lspkind")
+		local window = require("lspconfig.ui.windows")
 
-    window.default_options.border = "single"
+		window.default_options.border = "single"
 
-    lsp.on_attach(function(client, bufnr)
-      local opts = { buffer = bufnr, remap = false }
+		lsp.on_attach(function(client, bufnr)
+			local opts = { buffer = bufnr, remap = false }
 
-      vim.keymap.set("n", "gr", function()
-        vim.lsp.buf.references()
-      end, opts, { desc = "LSP Goto Reference" })
+			vim.keymap.set("n", "gr", function()
+				vim.lsp.buf.references()
+			end, opts, { desc = "LSP Goto Reference" })
 
-      vim.keymap.set("n", "<leader>gd", function()
-        vim.lsp.buf.definition()
-      end, opts, { desc = "LSP Goto Definition" })
+			vim.keymap.set("n", "<leader>gd", function()
+				vim.lsp.buf.definition()
+			end, opts, { desc = "LSP Goto Definition" })
 
-      vim.keymap.set("n", "<leader>h", function()
-        vim.lsp.buf.hover()
-      end, opts, { desc = "LSP Hover" })
+			vim.keymap.set("n", "<leader>h", function()
+				vim.lsp.buf.hover()
+			end, opts, { desc = "LSP Hover" })
 
-      vim.keymap.set("n", "<leader>ca", function()
-        vim.lsp.buf.code_action()
-      end, opts, { desc = "LSP Code Action" })
-    end)
+			vim.keymap.set("n", "<leader>ca", function()
+				vim.lsp.buf.code_action()
+			end, opts, { desc = "LSP Code Action" })
+		end)
 
-    require("mason").setup({})
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "tsserver",
-        "eslint",
-        "rust_analyzer",
-        "lua_ls",
-        "jdtls",
-        "pyright",
-        "gopls"
-      },
-      handlers = {
-        lsp.default_setup,
-        lua_ls = function()
-          local lua_opts = lsp.nvim_lua_ls()
-          require("lspconfig").lua_ls.setup(lua_opts)
-        end,
+		require("mason").setup({})
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"ts_ls",
+				"eslint",
+				"rust_analyzer",
+				"lua_ls",
+				"jdtls",
+				"pyright",
+				"gopls",
+			},
+			handlers = {
+				lsp.default_setup,
+				lua_ls = function()
+					local lua_opts = lsp.nvim_lua_ls()
+					require("lspconfig").lua_ls.setup(lua_opts)
+				end,
+			},
+		})
 
-      },
-    })
+		local cmp = require("cmp")
 
-    local cmp = require("cmp")
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-    require("luasnip.loaders.from_vscode").lazy_load()
+		-- `/` cmdline setup.
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
 
-    -- `/` cmdline setup.
-    cmp.setup.cmdline("/", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
+		-- `:` cmdline setup.
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{
+					name = "cmdline",
+					option = {
+						ignore_cmds = { "Man", "!" },
+					},
+				},
+			}),
+		})
+		cmp.setup({
+			preselect = "",
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
 
-    -- `:` cmdline setup.
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        {
-          name = "cmdline",
-          option = {
-            ignore_cmds = { "Man", "!" },
-          },
-        },
-      }),
-    })
-    cmp.setup({
-      preselect = "",
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path", max_item_count = 5 },
+			},
 
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path",    max_item_count = 5 },
-      },
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
 
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
+			mapping = cmp.mapping.preset.insert({
+				["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+				["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+				["<C-e>"] = cmp.mapping({
+					i = cmp.mapping.abort(),
+					c = cmp.mapping.close(),
+				}),
+				-- Accept currently selected item. If none selected, `select` first item.
+				-- Set `select` to `false` to only confirm explicitly selected items.
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+			}),
 
-      mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-        ["<C-e>"] = cmp.mapping({
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        }),
-        -- Accept currently selected item. If none selected, `select` first item.
-        -- Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      }),
-
-      formatting = {
-        format = lspkind.cmp_format({
-          mode = "symbol_text",
-          menu = {
-            buffer = "",
-            nvim_lsp = "",
-            luasnip = "",
-            nvim_lua = "",
-          },
-          maxwidth = 50,
-          ellipsis = "...",
-          symbol_map = {
-            Text = "󰉿",
-            Method = "󰆧",
-            Function = "󰆧",
-            Constructor = "",
-            Field = "󰜢",
-            Variable = "󰀫",
-            Class = "󰠱",
-            Interface = "",
-            Module = "",
-            Property = "󰜢",
-            Unit = "󰑭",
-            Value = "󰎠",
-            Enum = "",
-            Keyword = "󰌋",
-            Snippet = "",
-            Color = "󰏘",
-            File = "󰈙",
-            Reference = "󰈇",
-            Folder = "󰉋",
-            EnumMember = "",
-            Constant = "󰏿",
-            Struct = "󰙅",
-            Event = "",
-            Operator = "󰆕",
-            TypeParameter = "",
-          },
-        }),
-      },
-    })
-  end,
+			formatting = {
+				format = lspkind.cmp_format({
+					mode = "symbol_text",
+					menu = {
+						buffer = "",
+						nvim_lsp = "",
+						luasnip = "",
+						nvim_lua = "",
+					},
+					maxwidth = 50,
+					ellipsis = "...",
+					symbol_map = {
+						Text = "󰉿",
+						Method = "󰆧",
+						Function = "󰆧",
+						Constructor = "",
+						Field = "󰜢",
+						Variable = "󰀫",
+						Class = "󰠱",
+						Interface = "",
+						Module = "",
+						Property = "󰜢",
+						Unit = "󰑭",
+						Value = "󰎠",
+						Enum = "",
+						Keyword = "󰌋",
+						Snippet = "",
+						Color = "󰏘",
+						File = "󰈙",
+						Reference = "󰈇",
+						Folder = "󰉋",
+						EnumMember = "",
+						Constant = "󰏿",
+						Struct = "󰙅",
+						Event = "",
+						Operator = "󰆕",
+						TypeParameter = "",
+					},
+				}),
+			},
+		})
+	end,
 }
