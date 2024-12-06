a22dfdc0db364ed56a94fbff827d9ffa20964894
felipe <felipe.baleche2@gmail.com>
2024-12-06 11:23:48 -0300

    added folding bind in nvim config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,39 +1,43 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        pickers = {
-          find_files = {
-            hidden = true, --Set hidden files to show up
-            no_ignore = true,
-          }
-        }
-      })
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						hidden = true, --Set hidden files to show up
+						no_ignore = true,
+					},
+				},
+			})
 
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "S", builtin.find_files, {})
-      vim.keymap.set("n", "L", builtin.live_grep, {})
-    end,
-  },
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "TelescopeResults",
+				command = "setlocal nofoldenable",
+			})
 
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-        load_extension = {
-          "harpoon",
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "S", builtin.find_files, {})
+			vim.keymap.set("n", "L", builtin.live_grep, {})
+		end,
+	},
 
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+				load_extension = {
+					"harpoon",
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
