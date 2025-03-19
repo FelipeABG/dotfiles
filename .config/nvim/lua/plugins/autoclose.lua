a48481adafc7c43b08a3d60cefe3c54aa89b706f
felipe <felipe.baleche2@gmail.com>
2025-03-19 11:44:22 -0300

    autopair change

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -3,18 +3,18 @@ return {
 	config = function()
 		require("autoclose").setup({
 			keys = {
-				["("] = { escape = false, close = false, pair = "()" },
-				["["] = { escape = false, close = false, pair = "[]" },
-				["{"] = { escape = false, close = false, pair = "{}" },
+				["("] = { escape = false, close = true, pair = "()" },
+				["["] = { escape = false, close = true, pair = "[]" },
+				["{"] = { escape = false, close = true, pair = "{}" },
 
-				[">"] = { escape = true, close = false, pair = "<>" },
-				[")"] = { escape = true, close = false, pair = "()" },
-				["]"] = { escape = true, close = false, pair = "[]" },
-				["}"] = { escape = true, close = false, pair = "{}" },
+				[">"] = { escape = true, close = true, pair = "<>" },
+				[")"] = { escape = true, close = true, pair = "()" },
+				["]"] = { escape = true, close = true, pair = "[]" },
+				["}"] = { escape = true, close = true, pair = "{}" },
 
-				['"'] = { escape = true, close = false, pair = '""' },
-				["'"] = { escape = true, close = false, pair = "''" },
-				["`"] = { escape = true, close = false, pair = "``" },
+				['"'] = { escape = true, close = true, pair = '""' },
+				["'"] = { escape = true, close = true, pair = "''" },
+				["`"] = { escape = true, close = true, pair = "``" },
 			},
 			options = {
 				disabled_filetypes = { "text" },
