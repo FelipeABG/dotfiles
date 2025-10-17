2686dc0742083d4b35a7c35abe5dd0e66145b9e2
Felipe <felipe.baleche2@gmail.com>
2025-10-17 09:45:27 -0300

    style: update tab width

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.scrolloff = 10
