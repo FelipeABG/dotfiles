e16692425f3cd4e036937ef777f166e7685ebde3
Felipe <felipe.baleche2@gmail.com>
2026-03-14 10:01:04 -0300

    issue #1 - change live_grep picker to show only file name on result section

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,24 +1,17 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
+        version = "*",
         dependencies = {
             "nvim-lua/plenary.nvim",
             { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         },
         config = function()
-            require("telescope").setup({
-                pickers = {
-                    find_files = {
-                        hidden = true, --Set hidden files to show up
-                        no_ignore = true,
-                    },
-                },
-                extensions = {
-                    fzf = {},
-                    ["ui-select"] = {},
-                },
-            })
+            local telescope = require("telescope")
+            local builtin = require("telescope.builtin")
+            local make_entry = require("telescope.make_entry")
+            local devicons = require("nvim-web-devicons")
+            local entry_display = require("telescope.pickers.entry_display")
 
             local file_ignore_patterns = {
                 "yarn%.lock",
@@ -38,33 +31,74 @@ return {
                 "tmux/plugins"
             }
 
-            require("telescope").load_extension("fzf")
+            local displayer = entry_display.create({
+                separator = " ",
+                items = {
+                    { width = 2 },
+                    { remaining = true },
+                },
+            })
+
+            local grep_filename_only_entry_maker = function(opts)
+                local original = make_entry.gen_from_vimgrep(opts)
+
+                return function(line)
+                    local entry = original(line)
+                    if not entry then
+                        return nil
+                    end
+                    entry.display = function(e)
+                        local filename = vim.fn.fnamemodify(e.filename, ":t")
+                        local icon, hl = devicons.get_icon(filename, nil, { default = true })
+
+                        return displayer({ { icon, hl }, string.format("%s:%d", e.filename, e.lnum) })
+                    end
+                    return entry
+                end
+            end
+
+            telescope.setup({
+                pickers = {
+                    find_files = {
+                        hidden = true,
+                        no_ignore = true,
+                    },
+                },
+                extensions = {
+                    fzf = {},
+                    ["ui-select"] = {},
+                },
+            })
+
+            telescope.load_extension("fzf")
 
             vim.api.nvim_create_autocmd("FileType", {
                 pattern = "TelescopeResults",
                 command = "setlocal nofoldenable",
             })
-            local builtin = require("telescope.builtin")
 
             vim.keymap.set("n", "S", function()
-                builtin.find_files({ file_ignore_patterns = file_ignore_patterns })
+                builtin.find_files({
+                    file_ignore_patterns = file_ignore_patterns,
+                })
             end, {})
 
             vim.keymap.set("n", "L", function()
                 builtin.live_grep({
                     file_ignore_patterns = file_ignore_patterns,
+                    entry_maker = grep_filename_only_entry_maker({}),
                     additional_args = function()
                         return { "--hidden" }
                     end,
                 })
             end, {})
 
-            vim.keymap.set('n', '<leader>cfg', function()
-                require('telescope.builtin').find_files({
+            vim.keymap.set("n", "<leader>cfg", function()
+                builtin.find_files({
                     prompt_title = "Dotfiles",
                     cwd = "~/dotfiles",
                     hidden = true,
-                    file_ignore_patterns = file_ignore_patterns
+                    file_ignore_patterns = file_ignore_patterns,
                 })
             end, { desc = "Open dotfiles" })
         end,
