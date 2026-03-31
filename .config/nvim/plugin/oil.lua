ae84078d76f4d4cb9fc35bfbeaf37f19c4e2c382
Felipe <felipe.baleche2@gmail.com>
2026-03-31 18:16:44 -0300

    Issue #10: Add oil plugin configuration

---
+++ .config/nvim/plugin/oil.lua
@@ -0,0 +1,23 @@
+vim.pack.add({"https://github.com/stevearc/oil.nvim"})
+
+-- Imports 
+local oil = require("oil")
+
+-- Configuration
+oil.setup({
+    float = {
+        padding = 2,
+        max_width = 80,
+        max_height = 20,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+    },
+    view_options = {
+        show_hidden = true,
+    }
+})
+
+-- Keybinds
+vim.keymap.set("n", "T", ":Oil --float<CR>")
