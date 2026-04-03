f780863fc1bd19312def191c457527e6f7c79b92
Felipe Augusto <felipe.baleche2@gmail.com>
2026-04-03 11:17:58 -0300

    Issue #15: Add autocmd to build plugin's dependencies

--- .config/nvim/lua/core/pack.lua
+++ .config/nvim/lua/core/pack.lua
@@ -13,3 +13,35 @@ vim.api.nvim_create_autocmd("PackChanged", {
         end
     end
 })
+
+-- Autocommands to build plugin dependencies after installing and before laoding it.
+-- Check the `vim.pack-events` section of the `vim.pack` documentation
+-- to understand why this is here and not on each plugin config file.
+vim.api.nvim_create_autocmd("PackChanged", {
+    callback = function(event)
+        local plugin_name, kind = event.data.spec.name, event.data.kind
+
+        if plugin_name == "telescope-fzf-native.nvim" and (kind == "install" or kind == "update") then
+            vim.system({ "make" }, { cwd = event.data.path }):wait()
+        end
+    end
+})
+
+vim.api.nvim_create_autocmd('PackChanged', {
+    callback = function(event)
+        local plugin_name, kind = event.data.spec.name, event.data.kind
+
+        if plugin_name == 'blink.cmp' and (kind == "install" or kind == "update") then
+            vim.system({ 'cargo', 'build', '--release' }, { cwd = event.data.path }):wait()
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd('PackChanged', {
+    callback = function(ev)
+        local name, kind = ev.data.spec.name, ev.data.kind
+        if name == 'nvim-treesitter' and kind == 'update' then
+            vim.cmd('TSUpdate')
+        end
+    end
+})
--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -4,16 +4,6 @@ vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
 local treesitter = require("nvim-treesitter")
 
 -- Configuration
-vim.api.nvim_create_autocmd('PackChanged', {
-    callback = function(ev)
-        local name, kind = ev.data.spec.name, ev.data.kind
-        if name == 'nvim-treesitter' and kind == 'update' then
-            if not ev.data.active then vim.cmd.packadd('nvim-treesitter') end
-            vim.cmd('TSUpdate')
-        end
-    end
-})
-
 treesitter.setup({
     sync_install = false,
     auto_install = true,
