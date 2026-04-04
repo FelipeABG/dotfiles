3cae757e8f8c54c86ce0c140f3c5dfeaa2ab3805
Felipe Augusto <felipe.baleche2@gmail.com>
2026-04-04 09:48:04 -0300

    Issue #12: Add keybinds to gitsigns functionality

--- .config/nvim/plugin/gitsigns.lua
+++ .config/nvim/plugin/gitsigns.lua
@@ -4,4 +4,32 @@ vim.pack.add({ "https://github.com/lewis6991/gitsigns.nvim" })
 local gitsigns = require("gitsigns")
 
 -- Configuration
-gitsigns.setup({})
+gitsigns.setup({
+    on_attach = function(bufnr)
+        vim.keymap.set("n", "gshi", function()
+            gitsigns.preview_hunk_inline()
+        end, { buf = bufnr })
+
+        vim.keymap.set("n", "]c", function()
+            gitsigns.nav_hunk("next")
+        end, { buf = bufnr })
+
+        vim.keymap.set("n", "[c", function()
+            gitsigns.nav_hunk("prev")
+        end, { buf = bufnr })
+
+        vim.keymap.set("n", "gsd", function()
+            gitsigns.diffthis(nil, {})
+            -- Move cursor to left window
+            vim.cmd('wincmd w')
+        end, { buf = bufnr })
+
+        vim.keymap.set("n", "gshr", function()
+            gitsigns.reset_hunk()
+        end, { buf = bufnr })
+
+        vim.keymap.set("n", "gshq", function()
+            gitsigns.setqflist(bufnr)
+        end, { buf = bufnr })
+    end
+})
